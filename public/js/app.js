cd1d6c97a31b4859bb7fd8802d0b649e7fd72070
Debo <debojyoti.banik@pwc.com>
2020-02-18 22:31:43 +0530

    Removed unnecessary console.log from app.js client

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,11 @@
-console.log('Client side javascript file is added!');
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    
+
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
