488fe251bc52a0aaa3239a682db260f0a13c0c46
Debo <debojyoti.banik@pwc.com>
2020-06-14 12:23:37 +0530

    adding webhook

--- src/app.js
+++ src/app.js
@@ -125,6 +125,56 @@ app.get('/locate', (req, res) => {
             })
 })
 
+app.get('/webhook', (req, res) => {
+    console.log('receive post req');
+    if(!req.body)
+        return res.sendStatus(400)
+    res.setHeader('ContentType', 'application/json')   
+    console.log('Got geo city parameter from DialogFlow', req
+    .body.queryResult.parameters['geo-city']);
+     
+    var city = req.body.queryResult.parameters['geo-city']
+    var w = getWeather(city)
+
+    let response = ""
+    let responseObject = {
+        "fulfillmentText": response,
+        "fulfillmentMessages": [{"text": {"text": [w]}}]
+    }
+    console.log(responseObject);
+    return res.json(responseObject)
+})
+
+function getWeather(address){
+    var result 
+    geocode(address, (error, { latitude, longitude, location} = {}) => {
+        if(error){
+            return res.send({
+                error: error
+            })
+        }
+        
+        //forecast(latitude, longitude, (error, { temperature,precipitationProbablity: precipProbability}) => {
+        
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error){
+                return res.send({
+                    error: error
+                })
+            }
+    
+            result = forecastData
+            // res.send({
+            //     forecast: forecastData,
+            //     location: location,
+            //     address: req.query.address
+            // })
+          })
+    })
+    
+    return result
+}
+
 app.get('/products', (req, res) => {
     if(!req.query.search){
         return res.send({
